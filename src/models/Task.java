4db2ad585b9e7765ef1bf4c295525e2d8af1e6ca
yuuki.nakazawa <2023dounyukensyu.071@gmail.com>
2023-05-09 09:45:11 +0900

    Index/NewServletの修正

--- src/models/Task.java
+++ src/models/Task.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAlltasks",
+        name = "getAllTasks",
         query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
     )
 })
